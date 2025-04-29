44ef0c7b7e62784b3f7fa7b5acfa17939d38de59
Misbah <misbah.s@consultant.qdsnet.com>
2025-04-30 01:48:08 +0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    apiUrl:'moc-bp-dev-cqg2fha9bmd8gaf3.westeurope-01.azurewebsites.net:8080/api/auth/',
-     apiwebUrl: 'moc-bp-dev-cqg2fha9bmd8gaf3.westeurope-01.azurewebsites.net:8080/api',
-     sliderimageUrl: 'moc-bp-dev-cqg2fha9bmd8gaf3.westeurope-01.azurewebsites.net:8080/sliderImages/',
-     homeimageUrl: 'moc-bp-dev-cqg2fha9bmd8gaf3.westeurope-01.azurewebsites.net:8080/homeImages/',
-     prodImages: 'moc-bp-dev-cqg2fha9bmd8gaf3.westeurope-01.azurewebsites.net:8080/prodImages/'
+    apiUrl:'moc-bp-dev-cqg2fha9bmd8gaf3.westeurope-01.azurewebsites.net/api/auth/',
+     apiwebUrl: 'moc-bp-dev-cqg2fha9bmd8gaf3.westeurope-01.azurewebsites.net/api',
+     sliderimageUrl: 'moc-bp-dev-cqg2fha9bmd8gaf3.westeurope-01.azurewebsites.net/sliderImages/',
+     homeimageUrl: 'moc-bp-dev-cqg2fha9bmd8gaf3.westeurope-01.azurewebsites.net/homeImages/',
+     prodImages: 'moc-bp-dev-cqg2fha9bmd8gaf3.westeurope-01.azurewebsites.net/prodImages/'
 };
